8a43d77bd2cb151e19522e82f5dc13004800c426
starlee <13253620092@163.com>
2021-10-05 20:51:27 +0800

    add new page

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+### 新建文章
+## hexo new hello-world
+
+### 重新部署
+## yarn run deploy
+## hexo deploy
+
+### 清空缓存
+## yarn run clean
+## hexo clean
---
+++ source/_posts/hello-world-1.md
@@ -0,0 +1,5 @@
+---
+title: hello star lee
+date: 2021-10-05 20:43:23
+tags:
+---
